0950fbeeab511b08e083b88aafb009b2920ca205
Thomas Günther <tom@toms-cafe.de>
2007-12-04 00:28:24 +0100

    Improved copyright and license information in the header of all source files

--- generator.cpp
+++ generator.cpp
@@ -1,9 +1,23 @@
 /*
- * Sudoku: A plugin for the Video Disk Recorder
+ * Sudoku: A plug-in for the Video Disk Recorder
  *
- * See the README file for copyright information and how to reach the author.
+ * Copyright (C) 2005-2007, Thomas Günther <tom@toms-cafe.de>
  *
- * $Id: generator.cpp 11 2005-10-28 01:00:01Z tom $
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * $Id: generator.cpp 106 2007-12-03 23:28:24Z tom $
  */
 
 #define USE_RAND
