58141c746eb51e92d1c2350788b918932b8b7f92
Thomas Günther <tom@toms-cafe.de>
2008-08-30 02:02:08 +0200

    Set version number and release date for version 0.3.1

--- sudoku.cpp
+++ sudoku.cpp
@@ -17,7 +17,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * $Id: sudoku.cpp 146 2008-07-27 00:41:20Z tom $
+ * $Id: sudoku.cpp 152 2008-08-30 00:02:08Z tom $
  */
 
 #include "sudoku.h"
@@ -34,7 +34,7 @@ namespace SudokuPlugin
 {
 
   /** Version number of the plugin */
-  static const char* VERSION = "0.3.0";
+  static const char* VERSION = "0.3.1";
 
   /** Short description of the plugin's purpose */
   static const char* DESCRIPTION =
